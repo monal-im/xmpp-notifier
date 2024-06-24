ec08c4ce983bd4a9987e59fd26b12c5d0947b122
Thilo Molitor <thilo@eightysoft.de>
2024-06-24 04:18:32 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd "$(dirname "$0")"
 go get -d github.com/FluuxIO/go-xmpp
 go install github.com/FluuxIO/go-xmpp
 go run /notifier/main.go "${INPUT_SERVER_HOST}" "${INPUT_RECIPIENT}" "${INPUT_JID}" "${INPUT_PASSWORD}" "${INPUT_SERVER_PORT}" "${INPUT_MESSAGE}" "${INPUT_RECIPIENT_IS_ROOM}" "${INPUT_BOT_ALIAS}"
